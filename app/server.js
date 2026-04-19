b0d7f684e468350468dafce07fc8c1486bfb2588
roy <bluraya74@gmail.com>
2026-04-19 14:57:59 +0300

    test107

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const mySecret = process.env.SECRET_WORD || "No secret injected yet";
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello Infinity Labs!</h1>
+    <h1>Hello Infinity Labs!!</h1>
     <p>this app is running on EKS and was deployed via GitLab CI.</p>
     <p>The secret injected from Vault is: <strong>${mySecret}</strong></p>
   `);
